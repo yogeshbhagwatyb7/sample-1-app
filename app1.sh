9e03c5f9b0a5fbe74e4ca0526218d95820a697d5
yogeshbhagwatyb7 <157682980+yogeshbhagwatyb7@users.noreply.github.com>
2024-02-11 19:13:43 +0530

    Update app1.sh

--- app1.sh
+++ app1.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
 sudo su
+
+# Change directory to root
 cd /
-cd home/
+
+# Change directory to /home/
+cd /home/
+
+# Change directory to /home/ubuntu/
 cd ubuntu/
+
+# List files in the directory
 ls
+
+# Run the shell script file.sh
 ./file.sh
